e6e74ae7e1c5ccf33460632ebcb34386a1676750
suzuki_takuro <takuro.suzuki@drecom.co.jp>
2013-08-23 16:11:38 +0900

    ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Atnd::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
